8b14d2aead7a21957e001728b1a1122260b85f85
Anand <anand1911991@outlook.com>
2023-05-07 11:04:33 +0000

    solve git issue and include vim inside the devcontainer

---
+++ prerequisites.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir -p logs
+
+# Building dev container image
+echo "================================================"
+echo "Building dev container image"
+docker build \
+  -f docker/Dockerfile.devcontainer \
+  -t python-310-devcontainer docker/
+echo "Finished building devcontainer image"
+
+# Create the docker network for the stack
+echo "================================================"
+echo "Creating docker network"
+docker network create -d bridge hadoop_local_cluster
+echo "Finished creating docker network"
